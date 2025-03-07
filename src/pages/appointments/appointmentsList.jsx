eaa75e7f35b96a0d56bda0346d9365a2a7f0d9da
Vipin Sahu <vipinsahu1990@gmail.com>
2025-03-07 21:19:22 +0530

    added links on page to route

--- src/pages/appointments/appointmentsList.jsx
+++ src/pages/appointments/appointmentsList.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import AppointmentRouting from "../../components/RoutingButtons/AppointmentRouting";
 const AppointmentsList = () => {
   return (
-    <div>
-      appointment booking list
-    </div>
-  )
-}
+    <div className="min-h-screen flex items-center justify-center bg-white py-6 flex-col gap-12">
+      <AppointmentRouting/>
 
-export default AppointmentsList
+      <div className="w-full max-w-3xl bg-slate-700 p-6 rounded-lg shadow-lg space-y-6">
+        <h2 className="text-2xl font-semibold text-center text-white">
+          Appointment List
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default AppointmentsList;
