4a6ff7e23cf1aecc81c092618ca822bad3f4bd26
𝐀𝐌𝐎𝐋 𝐏𝐀𝐖𝐀𝐑 <24amolpawar@gmail.com>
2025-03-27 00:36:26 +0530

    Fixed The Form Data Submittion bug

--- src/pages/doctors/addDoctor.jsx
+++ src/pages/doctors/addDoctor.jsx
@@ -26,20 +26,26 @@ export default function AddDoctor() {
   const today = new Date().toISOString().split("T")[0];
 
   const handleChange = (e) => {
-    console.log("name-", e.target.name);
-    if (e.target.name === "mobile") {
-      setIsMobileValid(validateMobile(e.target.value));
+    const { name, value } = e.target;
+
+    // Handle mobile and email validation separately
+    if (name === "mobile") {
+      setIsMobileValid(validateMobile(value));
     }
-    if (e.target.name === "email") {
-      setIsValidEmail(validateEmail(e.target.value));
+    if (name === "email") {
+      setIsValidEmail(validateEmail(value));
     }
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   const handleShiftChange = (shiftIndex, field, value) => {
     const updatedShifts = [...formData.Shifts];
     updatedShifts[shiftIndex][field] = value;
-    setFormData({ ...formData, Shifts: updatedShifts });
+    setFormData((prevData) => ({ ...prevData, Shifts: updatedShifts }));
   };
 
   const handleDayToggle = (shiftIndex, day) => {
@@ -52,11 +58,19 @@ export default function AddDoctor() {
     } else {
       updatedShifts[shiftIndex].Days.push(day);
     }
-    setFormData({ ...formData, Shifts: updatedShifts });
+    setFormData((prevData) => ({ ...prevData, Shifts: updatedShifts }));
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevents the default form submission
+
+    // Validate mobile and email before submitting
+    if (!isMobileValid || !isValidEmail) {
+      console.log("Please fix validation errors.");
+      return; // Prevent form submission if validation fails
+    }
+
+    // Log the form data to check the values
     console.log("Form Submitted:", formData);
   };
 
@@ -65,20 +79,17 @@ export default function AddDoctor() {
       <AppointmentRouting pageName="Doctors" />
       <div className="w-full max-w-3xl bg-slate-700 p-6 rounded-lg shadow-lg space-y-6">
         <h2 className="text-2xl font-semibold text-center text-white">
-          Fill doctors details
+          Fill doctor's details
         </h2>
         <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4">
           {/* First Name */}
           <div>
-            <label
-              htmlFor="firstName"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="FirstName" className="block text-sm font-medium text-white">
               First Name
             </label>
             <input
               type="text"
-              name="firstName"
+              name="FirstName"
               placeholder="First Name"
               value={formData.FirstName}
               onChange={handleChange}
@@ -87,15 +98,12 @@ export default function AddDoctor() {
           </div>
           {/* Last Name */}
           <div>
-            <label
-              htmlFor="lastName"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="LastName" className="block text-sm font-medium text-white">
               Last Name
             </label>
             <input
               type="text"
-              name="lastName"
+              name="LastName"
               placeholder="Last Name"
               value={formData.LastName}
               onChange={handleChange}
@@ -104,60 +112,43 @@ export default function AddDoctor() {
           </div>
           {/* Mobile */}
           <div>
-            <label
-              htmlFor="mobile"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="Mobile" className="block text-sm font-medium text-white">
               Mobile
             </label>
             <input
               type="tel"
-              name="mobile"
+              name="Mobile"
               placeholder="Mobile"
               value={formData.Mobile}
               onChange={handleChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-900 rounded-md"
               maxLength="10"
             />
-            {!isMobileValid && (
-              <h3 className="text-red-600">
-                Please enter valid mobile no.
-              </h3>
-            )}
+            {!isMobileValid && <h3 className="text-red-600">Please enter a valid mobile number.</h3>}
           </div>
           {/* Email */}
           <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="Email" className="block text-sm font-medium text-white">
               Email
             </label>
             <input
               type="email"
-              name="email"
+              name="Email"
               placeholder="Email"
               value={formData.Email}
               onChange={handleChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-900 rounded-md"
             />
-            {!isValidEmail && (
-              <h3 className="text-red-600">
-                Please enter valid Email.
-              </h3>
-            )}
+            {!isValidEmail && <h3 className="text-red-600">Please enter a valid email.</h3>}
           </div>
           {/* DOB */}
           <div>
-            <label
-              htmlFor="dob"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="DOB" className="block text-sm font-medium text-white">
               DOB
             </label>
             <input
               type="date"
-              name="dob"
+              name="DOB"
               value={formData.DOB}
               max={today}
               onChange={handleChange}
@@ -166,15 +157,12 @@ export default function AddDoctor() {
           </div>
           {/* Qualification */}
           <div>
-            <label
-              htmlFor="qualification"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="Qualification" className="block text-sm font-medium text-white">
               Qualification
             </label>
             <input
               type="text"
-              name="qualification"
+              name="Qualification"
               placeholder="Qualification"
               value={formData.Qualification}
               onChange={handleChange}
@@ -183,15 +171,12 @@ export default function AddDoctor() {
           </div>
           {/* Specialization */}
           <div>
-            <label
-              htmlFor="specialization"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="Specialization" className="block text-sm font-medium text-white">
               Specialization
             </label>
             <input
               type="text"
-              name="specialization"
+              name="Specialization"
               placeholder="Specialization"
               value={formData.Specialization}
               onChange={handleChange}
@@ -200,15 +185,12 @@ export default function AddDoctor() {
           </div>
           {/* Career Start Date */}
           <div>
-            <label
-              htmlFor="careerStartDate"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="CareerStartDate" className="block text-sm font-medium text-white">
               Career Start Date
             </label>
             <input
               type="date"
-              name="careerStartDate"
+              name="CareerStartDate"
               value={formData.CareerStartDate}
               onChange={handleChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-900 rounded-md"
@@ -216,14 +198,11 @@ export default function AddDoctor() {
           </div>
           {/* Gender */}
           <div>
-            <label
-              htmlFor="gender"
-              className="block text-sm font-medium text-white"
-            >
+            <label htmlFor="Gender" className="block text-sm font-medium text-white">
               Gender
             </label>
             <select
-              name="gender"
+              name="Gender"
               value={formData.Gender}
               onChange={handleChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-900 rounded-md"
@@ -238,9 +217,7 @@ export default function AddDoctor() {
         {/* Shift Details */}
         {formData.Shifts.map((shift, index) => (
           <div key={index} className="mt-4 p-4 border rounded bg-gray-800">
-            <h3 className="text-lg font-semibold text-white">
-              Shift-{index + 1} Working Days
-            </h3>
+            <h3 className="text-lg font-semibold text-white">Shift-{index + 1} Working Days</h3>
             <div className="flex space-x-2 mt-2 text-white">
               {daysOfWeek.map((day) => (
                 <label key={day} className="flex items-center">
@@ -259,18 +236,14 @@ export default function AddDoctor() {
               <input
                 type="time"
                 value={shift.StartTime}
-                onChange={(e) =>
-                  handleShiftChange(index, "StartTime", e.target.value)
-                }
+                onChange={(e) => handleShiftChange(index, "StartTime", e.target.value)}
                 className="p-2 border rounded bg-white text-black"
               />
               <span className="text-white mt-3">To</span>
               <input
                 type="time"
                 value={shift.EndTime}
-                onChange={(e) =>
-                  handleShiftChange(index, "EndTime", e.target.value)
-                }
+                onChange={(e) => handleShiftChange(index, "EndTime", e.target.value)}
                 className="p-2 border rounded bg-white text-black"
               />
             </div>
@@ -279,6 +252,7 @@ export default function AddDoctor() {
 
         {/* Submit Button */}
         <button
+          type="submit"
           onClick={handleSubmit}
           className="w-full mt-4 p-2 bg-indigo-600 text-white rounded hover:bg-indigo-700"
         >
