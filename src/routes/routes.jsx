d617fec0bba5eb9a517bfc8c08575ad7330fbb01
Mohit Sahu <125088141+mohitsahu21@users.noreply.github.com>
2025-06-01 23:53:03 +0530

    add new name navbar

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import routesConfig from "./routesConfig.json"; // Import the JSON config
 import { lazy, Suspense, useEffect } from "react";
 import LoginRegister from "../pages/LoginRegister/LoginRegister";
-import Navbar from "../components/headerNavbar/Navbar"; // Corrected path
+import HeaderNavbar from "../components/headerNavbar/HeaderNavbar"; // Corrected path
 import useAuthStore from "../store/authStore";
 import { supabase } from "../supabaseClient";
 const loadComponent = (componentName) => {
@@ -55,7 +55,7 @@ const PrivateRoute = ({ children }) => {
 
   return (
     <>
-      <Navbar />
+      <HeaderNavbar />
       {children}
     </>
   );
