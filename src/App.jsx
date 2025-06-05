bcb8c7656976d24dad7efd61cfd21d2a14d997b8
Vipin Sahu <vipinsahu1990@gmail.com>
2025-06-05 23:47:34 +0530

    capital componentAppRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import AppRoutes from "./routes/AppRoutes";
 
-import AppRoutes from '../src/routes/Routes';
+
 function App() {
   return (
     <div className="w-full h-full bg-blue text-red relative">
-      <AppRoutes/>
+      <AppRoutes />
     </div>
   );
 }
