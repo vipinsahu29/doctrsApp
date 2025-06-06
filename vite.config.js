85758bbf33c84190dbc11118fa2274fb5c5593d1
Vipin Sahu <vipinsahu1990@gmail.com>
2025-06-06 22:53:49 +0530

    Fix formatting and add missing newlines in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import tailwindcss from 'tailwindcss'
+import tailwindcss from "tailwindcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,9 +9,12 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
-  server:{
-    proxy:{
-      '/api':''
-    }
-  }
+  server: {
+    proxy: {
+      "/api": "",
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 });
