83e801dfe064a7efe405e14179684b49263742c3
Vipin Sahu <vipinsahu1990@gmail.com>
2025-06-06 10:57:47 +0530

    Rename issue2 patient List added

---
+++ src/pages/patients/PatientsList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppointmentsList from '../appointments/AppointmentsList'
+
+const patientsList = () => {
+  return (
+    <div>
+        <AppointmentsList source={"Patients"}/>      
+    </div>
+  )
+}
+
+export default patientsList
